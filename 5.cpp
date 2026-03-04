7b834dbef1245df73f71555eb522f9f159d673e9
amina041 <happykhan3002@gmail.com>
2026-03-04 20:05:15 +0600

    solution 1 to 6

---
+++ 5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1, num2, product;
+
+    cout << "Enter two floating-point numbers: ";
+    cin >> num1 >> num2;
+
+    product = num1 * num2;
+
+    cout << "Product = " << product;
+    return 0;
+}
