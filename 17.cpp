d7a530a5321a7e422d84d0d4ad0fb5bfd52ada1d
amina041 <happykhan3002@gmail.com>
2026-03-10 19:20:36 +0600

    solve 16 to 20

---
+++ 17.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch(op) {
+        case '+':
+            cout << "Result = " << num1 + num2;
+            break;
+
+        case '-':
+            cout << "Result = " << num1 - num2;
+            break;
+
+        case '*':
+            cout << "Result = " << num1 * num2;
+            break;
+
+        case '/':
+            cout << "Result = " << num1 / num2;
+            break;
+
+        default:
+            cout << "Invalid Operator";
+    }
+
+    return 0;
+}
